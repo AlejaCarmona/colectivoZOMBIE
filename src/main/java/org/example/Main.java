f67137176bb384e9b7611f121373abe3b14dc761
desarrollo <desarrollo@academicos.cesde.edu.co>
2025-07-01 06:55:55 -0500

    feat titulo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,27 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        Scanner keyEntry=new Scanner(System.in);
+        System.out.println("***** RESIDENT EVIL CODE BY NO ONES*****" );
 
+        //PERSONAJES: leon s kennedy
+        //nombre
+        // edad
+        // cantidad de vida
+
+        //Personaje: MR X (TITAN)
+        //nombre
+        //virus que lo infecto
+        //cantidad de vida
+
+        //Definir variables
+        
 
     }
 }
